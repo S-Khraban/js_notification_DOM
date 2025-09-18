6677b496ef37084e101b2bf250765dc60f1a3c90
S-Khraban <s.hraban@i.ua>
2025-09-18 16:26:37 +0300

    _fix top and right const_

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,54 +1,50 @@
 'use strict';
 
-const pushNotification = (posTop, posRight, title, description, type) => {
+const pushNotification = (coordinates, title, description, type) => {
+  const { top: topPos, right: rightPos } = coordinates;
+
   const root = document.createElement('div');
+
   root.className = `notification ${type}`;
 
-  // Позиціювання
-  root.style.top = `${posTop}px`;
-  root.style.right = `${posRight}px`;
+  root.style.top = `${topPos}px`;
+  root.style.right = `${rightPos}px`;
 
-  // Заголовок
   const h2 = document.createElement('h2');
+
   h2.className = 'title';
   h2.textContent = title;
 
-  // Опис
   const p = document.createElement('p');
+
   p.textContent = description;
 
   root.append(h2, p);
   document.body.appendChild(root);
 
-  // Ховаємо через 2 секунди
   setTimeout(() => {
     root.style.display = 'none';
   }, 2000);
 };
 
-// зробимо доступним для тестів
 window.pushNotification = pushNotification;
 
-// Виклики
 pushNotification(
-  10,
-  10,
+  { top: 10, right: 10 },
   'Title of Success message',
   'Message example.\n Notification should contain title and description.',
   'success',
 );
 
 pushNotification(
-  150,
-  10,
+  { top: 150, right: 10 },
   'Title of Error message',
   'Message example.\n Notification should contain title and description.',
   'error',
 );
 
 pushNotification(
-  290,
-  10,
+  { top: 290, right: 10 },
   'Title of Warning message',
   'Message example.\n Notification should contain title and description.',
   'warning',
