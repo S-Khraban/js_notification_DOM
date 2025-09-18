46b43d136e15ee3dfafe7d79941fd16f6aa4ace8
S-Khraban <s.hraban@i.ua>
2025-09-18 15:54:17 +0300

    _Solution for Notification DOM task_

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const root = document.createElement('div');
+
+  root.className = `notification ${type}`;
+
+  root.setAttribute('style', `top: ${posTop}px; right: ${posRight}px;`);
+
+  const h2 = document.createElement('h2');
+
+  h2.className = 'title';
+  h2.textContent = title;
+
+  const p = document.createElement('p');
+
+  p.textContent = description;
+
+  root.append(h2, p);
+  document.body.appendChild(root);
+
+  setTimeout(() => {
+    root.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
